eb29f3623082368689bc247537ff0300504e6af5
Oleksandr Zvizdohliad <66206894+1ZvEzD1K1@users.noreply.github.com>
2021-07-05 18:49:53 +0300

    Js Training

---
+++ aimtrener/app.js
@@ -0,0 +1,94 @@
+const startBtn = document.querySelector("#start");
+const screens = document.querySelectorAll(".screen");
+const timeList = document.querySelector("#time-list");
+const timeEl = document.querySelector("#time");
+const board = document.querySelector("#board");
+let time = 0;
+let score = 0;
+
+startBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  screens[0].classList.add("up");
+});
+
+timeList.addEventListener("click", (e) => {
+  if (e.target.classList.contains("time-btn")) {
+    time = parseInt(e.target.getAttribute("data-time"));
+    screens[1].classList.add("up");
+    startGame();
+  }
+});
+
+board.addEventListener("click", (e) => {
+  if (e.target.classList.contains("circle")) {
+    score++;
+    e.target.remove();
+    createRandomCircle();
+  }
+});
+
+function startGame() {
+  setInterval(decreaseTime, 1000);
+  createRandomCircle();
+  setTime(time);
+}
+
+function decreaseTime() {
+  if (time === 0) {
+    finishGame();
+  } else {
+    let curent = --time;
+    if (curent < 10) {
+      curent = `0${curent}`;
+    }
+    setTime(curent);
+  }
+}
+
+function setTime(params) {
+  timeEl.innerHTML = `00:${params}`;
+}
+
+function finishGame() {
+  timeEl.parentNode.classList.add("hide");
+  board.innerHTML = `<h1>Your score: <span class="primary">${score}</span></h1>`;
+}
+
+function createRandomCircle() {
+  const circle = document.createElement("div");
+  const size = getRandomNumber(10, 60);
+  const { width, height } = board.getBoundingClientRect();
+  const x = getRandomNumber(0, width - size);
+  const y = getRandomNumber(0, height - size);
+
+  circle.classList.add("circle");
+  circle.style.width = `${size}px`;
+  circle.style.height = `${size}px`;
+  circle.style.top = `${y}px`;
+  circle.style.left = `${x}px`;
+  circle.style.background = getRandomColor();
+
+  board.append(circle);
+}
+
+function getRandomNumber(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+function getRandomColor() {
+  let r = Math.floor(Math.random() * 256);
+  let g = Math.floor(Math.random() * 256);
+  let b = Math.floor(Math.random() * 256);
+  let color = `#${r.toString(16)}${g.toString(16)}${b.toString(16)}`;
+  return color;
+}
+
+function Cheat() {
+  function tap() {
+    const circle = document.querySelector(".circle");
+    if (circle) {
+      circle.click();
+    }
+  }
+  setInterval(tap, 100)
+}
